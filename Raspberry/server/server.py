1985e55c22003379c8e0288957af4ae0fe8e4461
제동진 <43737240+tkroahdla@users.noreply.github.com>
2022-05-07 17:44:57 +0900

    우산 태그 인식 요청 삭제 , 솔레노이드 동작 코드 추가 -동진-
    우산 태그 인식 요청 삭제 , 솔레노이드 동작 코드 추가 -동진-

--- Raspberry/server/server.py
+++ Raspberry/server/server.py
@@ -1,3 +1,4 @@
+from solenoid import *
 import requests
 from flask import Flask, request, render_template
 
@@ -7,27 +8,15 @@ app = Flask(__name__)       # Flask라는 이름의 객체 생성
 def hello():                # /로 실행하면 호출되는 뷰함수
    return "Hello Flask!"    # 뷰함수는 반드시 retrun이 있어야한다.
 
-@app.route('/frontRfid')
+@app.route('/soleON')
 def qwer():
-    uid = request.args.get("uid")
-    print("앞쪽 RFID : "+uid)
-    
-    URL = 'http://59.0.129.219:8081/umb/frontRfid'
-    response = requests.get(URL)
-    response.status_code
-print(response.text)
-    return 0;
+    sole_ON()
+    return "asdf"
 
-@app.route('/backRfid')
+@app.route('/soleOFF')
 def asdf():
-    uid = request.args.get("uid")
-    print("앞쪽 RFID : "+uid)
-    
-    URL = 'http://59.0.129.219:8081/umb/backRfid'
-    response = requests.get(URL)
-    response.status_code
-    return"뒤쪽 RFID"
+    sole_OFF()
 
 
 if __name__ == "__main__":  # 직접 main을 실행시키기위한 조건
-   app.run(host="0.0.0.0", port = "8082")
+   app.run(host="172.30.1.49", port = "8082")
