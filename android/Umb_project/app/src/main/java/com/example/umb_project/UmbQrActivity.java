283076d3f6c7079d6a2c9ccacef3d657103d6d46
jin <tkroahdla@naver.com>
2022-05-09 11:10:16 +0900

    기능 추가 : QR 스캔 -동진-

---
+++ android/Umb_project/app/src/main/java/com/example/umb_project/UmbQrActivity.java
@@ -0,0 +1,14 @@
+package com.example.umb_project;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class UmbQrActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_umb_qr);
+    }
+}
