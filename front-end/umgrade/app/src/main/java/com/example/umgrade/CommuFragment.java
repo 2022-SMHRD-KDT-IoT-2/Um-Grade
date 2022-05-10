efd0c337e937db324dc974cbd9da097563840e68
yeeun-b <yeeun.park.417@gmail.com>
2022-05-10 17:46:29 +0900

    프래그먼트 생성 -예은-

---
+++ front-end/umgrade/app/src/main/java/com/example/umgrade/CommuFragment.java
@@ -0,0 +1,19 @@
+package com.example.umgrade;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class CommuFragment extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_commu, container, false);
+        return view;
+    }
+}
---
+++ front-end/umgrade/app/src/main/java/com/example/umgrade/MainFragment.java
@@ -0,0 +1,20 @@
+package com.example.umgrade;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class MainFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_main, container, false);
+        return view;
+    }
+}
