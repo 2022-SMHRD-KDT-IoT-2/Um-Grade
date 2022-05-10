b2d974de5ca0ab4fa43fb5248800f864b6ff2b76
koavocado <bg9608@naver.com>
2022-05-10 12:29:43 +0900

    [수정] 유저 매퍼, 서비스 수정(카드등록~포인트 추가) -성준-

--- back-end/Umb_project/src/main/java/kr/smhrd/mapper/UserMapper.java
+++ back-end/Umb_project/src/main/java/kr/smhrd/mapper/UserMapper.java
@@ -1,5 +1,6 @@
 package kr.smhrd.mapper;
 
+import java.util.HashMap;
 import java.util.List;
 
 import org.apache.ibatis.annotations.Select;
@@ -16,5 +17,17 @@ public interface UserMapper {
 	
 	//유저 로그인 
 	public User userLogin(User user);
+	
+	//유저 카드등록
+	public void updateUserBK(HashMap<String, String> map);
+	
+	//유저 카드등록 취소
+	public void updateUserBKUnused(String user_id);
+	
+	//유저 포인트 조회(결제금액 이상인지, 미만인지 판별)
+	public boolean isExistUserPoint(HashMap<String, Object> map);
+	
+	//유저 포인트 수정 (결제 이후 결제금액 차감(음수로 입력) + 쿠폰이나 리워드로 포인트 충전)
+	public void updateUserPoint(HashMap<String, Object> map);
 	
 }
--- back-end/Umb_project/src/main/java/kr/smhrd/service/UserService.java
+++ back-end/Umb_project/src/main/java/kr/smhrd/service/UserService.java
@@ -1,5 +1,6 @@
 package kr.smhrd.service;
 
+import java.util.HashMap;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -32,5 +33,27 @@ public class UserService {
 		return info;
 
 	}
+	
+	//유저 카드등록
+	public void updateUserBK(HashMap<String, String> map) {
+		mapper.updateUserBK(map);
+	}
+	
+	//유저 카드등록 취소
+	public void updateUserBKUnused(String user_id) {
+		mapper.updateUserBKUnused(user_id);
+	}
+	
+	//유저 포인트 조회(결제금액 이상인지, 미만인지 판별)
+	public boolean isExistUserPoint(HashMap<String, Object> map) {
+		return mapper.isExistUserPoint(map);
+	}
+	
+	//유저 포인트 수정 (결제 이후 결제금액 차감(음수로 입력) + 쿠폰이나 리워드로 포인트 충전)
+	public void updateUserPoint(HashMap<String, Object> map) {
+		mapper.updateUserPoint(map);
+	}
+	
+	
 
 }
