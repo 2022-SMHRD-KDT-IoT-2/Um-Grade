8b85ccd87f7899918be7d8ce5014cddce04c6d95
yeeun-b <yeeun.park.417@gmail.com>
2022-05-11 10:26:03 +0900

    Constraint Layout으로 변경  -예은-

--- front-end/umgrade/app/build/generated/data_binding_base_class_source_out/debug/out/com/example/umgrade/databinding/FragmentMainBinding.java
+++ front-end/umgrade/app/build/generated/data_binding_base_class_source_out/debug/out/com/example/umgrade/databinding/FragmentMainBinding.java
@@ -4,20 +4,27 @@ package com.example.umgrade.databinding;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.FrameLayout;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.viewbinding.ViewBinding;
+import androidx.viewbinding.ViewBindings;
 import com.example.umgrade.R;
 import java.lang.NullPointerException;
 import java.lang.Override;
+import java.lang.String;
 
 public final class FragmentMainBinding implements ViewBinding {
   @NonNull
   private final FrameLayout rootView;
 
-  private FragmentMainBinding(@NonNull FrameLayout rootView) {
+  @NonNull
+  public final Button button2;
+
+  private FragmentMainBinding(@NonNull FrameLayout rootView, @NonNull Button button2) {
     this.rootView = rootView;
+    this.button2 = button2;
   }
 
   @Override
@@ -43,10 +50,19 @@ public final class FragmentMainBinding implements ViewBinding {
 
   @NonNull
   public static FragmentMainBinding bind(@NonNull View rootView) {
-    if (rootView == null) {
-      throw new NullPointerException("rootView");
-    }
+    // The body of this method is generated in a way you would not otherwise write.
+    // This is done to optimize the compiled bytecode for size and performance.
+    int id;
+    missingId: {
+      id = R.id.button2;
+      Button button2 = ViewBindings.findChildViewById(rootView, id);
+      if (button2 == null) {
+        break missingId;
+      }
 
-    return new FragmentMainBinding((FrameLayout) rootView);
+      return new FragmentMainBinding((FrameLayout) rootView, button2);
+    }
+    String missingId = rootView.getResources().getResourceName(id);
+    throw new NullPointerException("Missing required view with ID: ".concat(missingId));
   }
 }
